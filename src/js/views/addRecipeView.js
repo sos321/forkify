687d3e995692047afab9a27f5ff6ffeab23acc22
Filip Kocian <filkocian@gmail.com>
2023-04-10 20:11:45 +0200

    fix upload recipe error message

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,4 +1,5 @@
 import View from './View';
+import icons from 'url:../../img/icons.svg';
 
 class AddRecipeView extends View {
   _parentContainer = document.querySelector('.upload');
@@ -17,6 +18,10 @@ class AddRecipeView extends View {
   toggleWindow() {
     this._overlay.classList.toggle('hidden');
     this._window.classList.toggle('hidden');
+
+    setTimeout(() => {
+      this._parentContainer.innerHTML = this._rebuildForm();
+    }, 500);
   }
 
   _addHandlers() {
@@ -24,6 +29,74 @@ class AddRecipeView extends View {
 
     this._buttonClose.addEventListener('click', this.toggleWindow.bind(this));
     this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _rebuildForm() {
+    return `
+    <div class="upload__column">
+    <h3 class="upload__heading">Recipe data</h3>
+    <label>Title</label>
+    <input required name="title" type="text" />
+    <label>URL</label>
+    <input required name="sourceUrl" type="text" />
+    <label>Image URL</label>
+    <input required name="image" type="text" />
+    <label>Publisher</label>
+    <input required name="publisher" type="text" />
+    <label>Prep time</label>
+    <input required name="cookingTime" type="number" />
+    <label>Servings</label>
+    <input required name="servings" type="number" />
+  </div>
+
+  <div class="upload__column">
+    <h3 class="upload__heading">Ingredients</h3>
+    <label>Ingredient 1</label>
+    <input
+      type="text"
+      required
+      name="ingredient-1"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 2</label>
+    <input
+      type="text"
+      name="ingredient-2"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 3</label>
+    <input
+      type="text"
+      name="ingredient-3"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 4</label>
+    <input
+      type="text"
+      name="ingredient-4"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 5</label>
+    <input
+      type="text"
+      name="ingredient-5"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+    <label>Ingredient 6</label>
+    <input
+      type="text"
+      name="ingredient-6"
+      placeholder="Format: 'Quantity,Unit,Description'"
+    />
+  </div>
+
+  <button class="btn upload__btn">
+    <svg>
+      <use href="${icons}#icon-upload-cloud"></use>
+    </svg>
+    <span>Upload</span>
+  </button>
+    `;
   }
 
   addHandlerUpload(handler) {
